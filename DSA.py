e9669b10014b36188e7bb73b1f78a8908f4e56d7
Rabinam <rabinbhandari070@gmail.com>
2024-03-18 21:47:20 +0545

    osh

--- DSA.py
+++ DSA.py
@@ -483,3 +483,18 @@ class Solution:
 sol=Solution()
 print(sol.addDigit(38))
         
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+
+class Solution:
+    def missingvalue(self,nums):
+        n = len(nums)
+        total_sum = n * (n + 1) // 2
+        array_sum = sum(nums)
+        return total_sum - array_sum
+    
+sol= Solution()
+print(sol.missingvalue([9,6,4,2,3,5,7,0,1])) 
